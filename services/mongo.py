d3d5b3a4e01ebadc6bab649ba4dc1ae597fee118
Devansh Khare <devanshkhare0@gmail.com>
2024-06-12 21:46:33 +0530

    pymongo helpers

---
+++ services/mongo.py
@@ -0,0 +1,26 @@
+import pymongo
+
+def add_file_to_queue(file_name):
+    try:
+        client = pymongo.MongoClient("mongodb://root:root@ais-mongodb:27017")
+        db = client["ais"]
+        collection = db["scheduled_files"]
+        collection2 = db["all_files"]
+        collection.insert_one({"file_name": file_name})
+        collection2.insert_one({"file_name": file_name})
+        return {"message": "File added to queue successfully!", "success": True}
+    except Exception as e:
+        return {"message": f"File addition to queue failed!{e}", "success": False}
+
+def get_file_from_queue():
+    try:
+        client = pymongo.MongoClient("mongodb://root:root@ais-mongodb:27017")
+        db = client["ais"]
+        collection = db["scheduled_files"]
+        files = []
+        for x in collection.find():
+            files.append(x["file_name"])
+        return files
+    except Exception as e:
+        return {"message": f"File retrieval from queue failed!{e}", "success": False}
+
